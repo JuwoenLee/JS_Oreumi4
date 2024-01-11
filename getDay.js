fd0a11d9ecb70bd6145423e2cbc95bd3d7a89063
LeeJuWon <woen1016@gmail.com>
2024-01-11 11:35:18 +0900

    switch문으로 요일 정보 출력하기

---
+++ getDay.js
@@ -0,0 +1,19 @@
+switch (new Date().getDay()) {
+    case 1 :
+        console.log("월요일");
+        break;
+    case 2 :
+        console.log("화요일");
+        break;
+    case 3 :
+        dconsole.log("수요일");
+        break;
+    case 4 :
+        console.log("목요일");
+        break;
+    case 5 :
+        console.log("금요일");
+        break;
+    default :
+        console.log("금금요일 주말이 뭐죠?");
+}
